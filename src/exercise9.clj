fb9ff6b7bc3abbd92dde87fb824b952a71317178
DamiCassinotti <damiancassinotti@gmail.com>
2020-11-25 19:45:02 -0300

    Added ej9

--- src/exercise9.clj
+++ src/exercise9.clj
@@ -1,11 +1,8 @@
-(ns exercise9)
+(ns exercise9
+	 (:require [clojure.string :refer [lower-case split]]))
 
-(defn word-count 
+(defn word-count
   "Returns a map where each key is a distinct word in a given sentence and each value is the number of occurences of that word in the sentence."
   [x]
-  (throw (Exception. "Not implemented."))
+  (into {} (map (fn [[key value]] [key (count value)]) (group-by identity (map lower-case (split x #"\W+")))))
 )
-
-
-
-
